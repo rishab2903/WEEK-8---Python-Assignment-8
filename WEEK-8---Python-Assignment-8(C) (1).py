89f8c289337015e4ceb13613c31fad61d84674e2
rishab2903 <54606079+rishab2903@users.noreply.github.com>
2020-04-08 21:16:50 -0400

    Add files via upload

---
+++ WEEK-8---Python-Assignment-8(C) (1).py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+import pandas as pd
+df = pd.read_csv("~/Downloads/learn-data-analysis-w-python-master/datasets/gradedata.csv")
+df.head()
+
+
+# In[66]:
+
+
+plt.scatter(df['grade'], df['hours'])
+
+
+# In[ ]:
+
+
+# The pattern we see is that for good grades, more number of hours were pt into study.
+# There is definately a relation between grades and the hours..
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
